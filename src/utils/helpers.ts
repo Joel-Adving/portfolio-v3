567eee11565fad04be1fab6601f8a6bf5b1cbe71
Joel-Adving <joel.adving@gmail.com>
2022-05-13 01:30:48 +0200

    added google analytics

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,3 +2,17 @@ export const mailTo = (to: string) => {
     // @ts-ignore
     window.location = 'mailto:' + to
 }
+
+// log the pageview with their URL
+export const pageview = (url: any) => {
+    // @ts-ignore
+    window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+    })
+}
+
+// log specific events happening.
+export const event = ({ action, params }: any) => {
+    // @ts-ignore
+    window.gtag('event', action, params)
+}
